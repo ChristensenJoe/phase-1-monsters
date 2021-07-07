2e256ad98931fcc2ec7863a34ec49b74ac7182f7
Joe Christensen <joechristensenwork@gmail.com>
2021-07-07 15:50:41 -0700

    Added form functionality

--- js/index.js
+++ js/index.js
@@ -1,32 +1,76 @@
+let currentPage = 1;
+
 document.addEventListener('DOMContentLoaded', () => {
-first50()
+    load50(currentPage);
+    createForm();
+    formEventListender();
+});
 
-
-})
-
-function first50() {
-    fetch ("http://localhost:3000/monsters/?_limit=50")
+function load50(page) {
+    document.querySelector("#monster-container").innerHTML = "";
+    fetch (`http://localhost:3000/monsters/?_limit=50&_page=${page}`)
     .then(res => res.json())
     .then(json => {
         for (monster in json) {
-            renderMonster(json[monster])
+            renderMonster(json[monster]);
         }
-    })
+    });
 }
 
-function renderMonster(monst){
-    let monstContainer = document.querySelector("#monster-container")
-    let monstName = document.createElement('h1')
-    let monstAge = document.createElement('p')
-    let monstDesc = document.createElement('p')
+function createForm() {
+    let formContainer = document.querySelector("#create-monster");
 
-    monstName.textContent = monst.name
-    monstAge.textContent = `Age: ${monst.age}`
-    monstDesc.textContent = `Bio: ${monst.description}`
+    let form = document.createElement("form");
+    let nameField = document.createElement("input");
+    let ageField = document.createElement("input");
+    let descField = document.createElement("input");
+    let submitButton = document.createElement("button");
 
-    monstContainer.append(monstName, monstAge, monstDesc)
+    form.id = "createMonster";
 
+    nameField.id = "nameField";
+    nameField.type = "text";
+    nameField.placeholder = "Name...";
+    ageField.id = "ageField";
+    ageField.type = "text";
+    ageField.placeholder = "Age...";
+    descField.id = "descField";
+    descField.type = "text";
+    descField.placeholder = "Bio..."
+    submitButton.textContent = "Create Monster Button";
+    submitButton.type = "submit";
+    submitButton.form = "createMonster";
 
+    form.append(nameField, ageField, descField, submitButton);
+    formContainer.append(form);
+}
 
+function formEventListender() {
+    document.querySelector("#createMonster").addEventListener("submit", (event) => {
+        event.preventDefault();
+        let monstSubmit = {};
+        monstSubmit.name = event.target.nameField.value;
+        monstSubmit.age = event.target.ageField.value;
+        monstSubmit.description = event.target.descField.value;
+        console.log("test");
+        renderMonster(monstSubmit, "top");
+    });
+}
 
+function renderMonster(monst, position="bottom"){
+    let monstContainer = document.querySelector("#monster-container");
+    let monstName = document.createElement('h1');
+    let monstAge = document.createElement('p');
+    let monstDesc = document.createElement('p');
+
+    monstName.textContent = monst.name;
+    monstAge.textContent = `Age: ${monst.age}`;
+    monstDesc.textContent = `Bio: ${monst.description}`;
+
+    if(position === "top") {
+        monstContainer.prepend(monstName, monstAge, monstDesc);
+    }
+    else if(position === "bottom") {
+        monstContainer.append(monstName, monstAge, monstDesc);
+    }
 }
